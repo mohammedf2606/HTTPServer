e79ff01a2893752d5ee44c0ad8fcfad3ef7476fd
Fahim Mohammed <mohammedf2606@gmail.com>
2018-11-09 09:03:59 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,8 @@ import threading
 class Handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        filename = self.path.rsplit('/', 1)[-1]
         # print(filename)
         self.send_response(200)
-        if filename[-4:] == '.css':
-            self.send_header('Content-type', 'text/css')
-        elif filename[-5:] == '.json':
-            self.send_header('Content-type', 'application/javascript')
-        elif filename[-3:] == '.js':
-            self.send_header('Content-type', 'application/javascript')
-        elif filename[-4:] == '.ico':
-            self.send_header('Content-type', 'image/x-icon')
-        else:
-            self.send_header('Content-type', 'text/html')
         self.end_headers()
         message = threading.currentThread().getName()
         message += '\n'
